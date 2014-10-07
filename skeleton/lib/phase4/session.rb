b77a3391154372ff8e20a655e4e98583ea88a2bb
Matthew Kevins <mmkevins@yahoo.com>
2014-10-07 17:14:12 -0400

    Finish phase IV.

--- skeleton/lib/phase4/session.rb
+++ skeleton/lib/phase4/session.rb
@@ -3,20 +3,34 @@ require 'webrick'
 
 module Phase4
   class Session
+
+    COOKIE_NAME = "_rails_lite_app"
+
     # find the cookie for this app
     # deserialize the cookie into a hash
     def initialize(req)
+
+      cookie_index = req.cookies.find_index do |cookie|
+        cookie.name == COOKIE_NAME
+      end
+
+      our_cookie = cookie_index.nil? ? nil : req.cookies[cookie_index]
+
+      @cookie = our_cookie.nil? ? {} : JSON.parse(our_cookie.value)
     end
 
     def [](key)
+      @cookie[key]
     end
 
     def []=(key, val)
+      @cookie[key] = val
     end
 
     # serialize the hash into json and save in a cookie
     # add to the responses cookies
     def store_session(res)
+      res.cookies << WEBrick::Cookie.new(COOKIE_NAME, @cookie.to_json)
     end
   end
 end
