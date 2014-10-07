6e3f85bee19c02be4023ab99121dc5ffe36b1821
Matthew Kevins <mmkevins@yahoo.com>
2014-10-07 12:54:30 -0400

    Finish phase I.

--- skeleton/bin/p01_basic_server.rb
+++ skeleton/bin/p01_basic_server.rb
@@ -1,5 +1,20 @@
 require 'webrick'
 
+root = File.expand_path '~/public_html'
+server = WEBrick::HTTPServer.new :Port => 3000, :DocumentRoot => root
+
+server.mount_proc '/' do |req, res|
+
+  res.content_type = "text/text"
+  res.body = req.path
+  #res.body = 'Hello, world!'
+
+end
+
+trap ('INT') { server.shutdown }
+
+server.start
+
 # http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick.html
 # http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/HTTPRequest.html
 # http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/HTTPResponse.html
