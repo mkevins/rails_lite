4c7414792a18aabc633d1c5350753fa7ad7346f3
Matthew Kevins <mmkevins@yahoo.com>
2014-10-07 14:20:35 -0400

    Finish phase II.

--- skeleton/lib/phase2/controller_base.rb
+++ skeleton/lib/phase2/controller_base.rb
@@ -4,20 +4,31 @@ module Phase2
 
     # Setup the controller
     def initialize(req, res)
+      @req = req
+      @res = res
     end
 
     # Helper method to alias @already_built_response
     def already_built_response?
+      !!@already_built_response
     end
 
     # Set the response status code and header
     def redirect_to(url)
+      raise "Hell" if @already_built_response
+      self.res.status = 302
+      self.res.header["location"] = url
+      @already_built_response = true
     end
 
     # Populate the response with content.
     # Set the response's content type to the given type.
     # Raise an error if the developer tries to double render.
     def render_content(content, type)
+      raise "Hell" if @already_built_response
+      self.res.content_type = type
+      self.res.body = content
+      @already_built_response = true
     end
   end
 end
