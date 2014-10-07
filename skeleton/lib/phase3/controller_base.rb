bab45b72cd4fb99f494587ed12613a1401661886
Matthew Kevins <mmkevins@yahoo.com>
2014-10-07 14:57:34 -0400

    Finish phase III.

--- skeleton/lib/phase3/controller_base.rb
+++ skeleton/lib/phase3/controller_base.rb
@@ -1,5 +1,6 @@
 require_relative '../phase2/controller_base'
 require 'active_support/core_ext'
+require 'active_support/inflector'
 require 'erb'
 
 module Phase3
@@ -7,6 +8,21 @@ module Phase3
     # use ERB and binding to evaluate templates
     # pass the rendered html to render_content
     def render(template_name)
+      template = ERB.new(
+        File.read(
+          view_file_name(template_name)
+        )
+      )
+      render_content(template.result(binding), "text/html")
+    end
+
+    private
+
+    def view_file_name(template_name)
+      view_file_name = "views/" +
+      #self.class.name.underscore.pluralize + "/" +
+      self.class.name.underscore + "/" +
+      template_name.to_s + ".html.erb"
     end
   end
 end
